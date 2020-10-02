d467ae91a79ccaf0a3c1c735f3d4dd8782a61c94
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-10-02 11:24:20 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -18,6 +18,13 @@ Enter a number: -3
 number= float(input("Enter a number: "))
 
 if number < 0 or number ==0:
-  print(str(number) + " is not a positive integer.")
-elif number > 0:
-  print(str(number) + " is not a positive integer.")
+  print(str(number) + " is not positive integer.")
+  quit()
+value=number%2
+if value==1 or value==0 and number!=0:
+  print(str(number) + " is a positive integer.")
+  quit()
+if number==0:
+  print("the number is zero")
+else:
+  print(str(number) + " is not positive integer.")
