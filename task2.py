56726c18ebdb78ad1e051964ef340eb5564630b8
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-10-02 12:52:53 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -19,10 +19,10 @@ Enter a number: 8
 8 is only a perfect cube.
 """
 number=int(input("Enter a number: "))
-cube = number ** (1/3)
-square = number ** 0.5
-sq2 = round(square,8)%2
-c2 = round(cube,8)%2
+cube=number**(1/3)
+square=number**0.5
+sq2=round(square,8)%2
+c2=round(cube,8)%2
 
 if (sq2==1 or sq2==0) and (c2==1 or c2==0):
   print(str(number) + " is both a perfect square and a perfect cube.")
